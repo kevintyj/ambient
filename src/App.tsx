a772df07938d4177deedac0eadd0cc78427c6c65
kevintyj <hello@kevintyj.com>
2022-06-08 19:07:05 -0400

    feat(init): Initialize required classes

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,9 @@ import {createSignal, onCleanup, onMount} from "solid-js";
 
 const App: Component = () => {
 
-  const [color, setColor] = createSignal([256, 256, 256]);
-
-  const [luminanceR, setLuminanceR] = createSignal(0);
-
-  onMount(() => {
-
-  });
-
-  onCleanup(() => {
-
-  });
-
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <div>
-
-        </div>
-        <h1>{ luminanceR() }</h1>
-      </header>
-    </div>
+    <>
+    </>
   );
 };
 
